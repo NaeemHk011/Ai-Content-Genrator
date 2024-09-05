c6a68821927224ceefc101c25792e60d30edd1a7
NaeemHk011 <itxkhaskheli@gmail.com>
2024-09-05 08:47:58 +0500

    front-end Completed Successfully

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import React from 'react'
-
 import FormSection from '../_components/FormSection';
 import OutputSection from '../_components/OutputSection';
 import { TEMPLATE } from '../../_components/TemplateSection';
 import Template from '@/app/(data)/Template';
+import { ArrowLeft } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 interface PROPS {
     params: {
         'template-slug': string
@@ -11,15 +14,19 @@ interface PROPS {
 }
 
 const CreateNewContent = (props: PROPS) => {
+    const generateAiContent = (formData: any) => { }
     const SelectedTemplate: TEMPLATE | undefined = Template?.find((item) => item.slug == props.params['template-slug'])
     return (
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-10 p-5'>
-            {/* from section  */}
-            <FormSection SelectedTemplate={SelectedTemplate} />
+        <div className='p-5'>
+            <Link href={'/dashboard'} > <Button><ArrowLeft />Back</Button></Link>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-10 p-5'>
+                {/* from section  */}
+                <FormSection SelectedTemplate={SelectedTemplate} userFormInput={(v: any) => generateAiContent(v)} />
 
-            {/* outputSection  */}
-            <div className='col-span-2'>
-            <OutputSection />
+                {/* outputSection  */}
+                <div className='col-span-2'>
+                    <OutputSection />
+                </div>
             </div>
         </div>
     )
--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
 const OutputSection = () => {
+  const editorRef: any = useRef();
   return (
-    <div>OutputDection</div>
+    <div className='bg-white shadow-lg border'>
+      <div className='flex justify-between items-center p-5'>
+        <h2 className='font-bold'>Your Result</h2>
+        <Button className='flex gap-2'><Copy className='w-4 h-4' />Copy</Button>
+      </div><Editor
+        ref={editorRef}
+        initialValue="Your Result will appear here!"
+        height='600px'
+        initialEditType="wysiwyg"
+        useCommandShortcut={true}
+        onChange={() => console.log(editorRef.current.getInstance().getMarkdown())}
+      /></div>
   )
 }
 
