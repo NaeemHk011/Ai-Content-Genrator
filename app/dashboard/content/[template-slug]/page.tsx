2f3e2ac0a1e65c6b6368fe9030dbabf41a6a952a
NaeemHk011 <itxkhaskheli@gmail.com>
2024-09-05 09:55:40 +0500

    Ai Implementation successfully

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import FormSection from '../_components/FormSection';
 import OutputSection from '../_components/OutputSection';
 import { TEMPLATE } from '../../_components/TemplateSection';
@@ -7,6 +7,7 @@ import Template from '@/app/(data)/Template';
 import { ArrowLeft } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { chatSession } from '@/utils/AiModal';
 interface PROPS {
     params: {
         'template-slug': string
@@ -14,18 +15,28 @@ interface PROPS {
 }
 
 const CreateNewContent = (props: PROPS) => {
-    const generateAiContent = (formData: any) => { }
+    const [aiOutput, setaiOutput] = useState<String>('')
+    const [loading, setloading] = useState(false);
+    const generateAiContent = async (formData: any) => {
+        setloading(true);
+        const SelectedPrompt = SelectedTemplate?.aiPrompt;
+        const FinalAiPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
+        const result = await chatSession.sendMessage(FinalAiPrompt)
+        console.log(result.response.text());
+        setaiOutput(result?.response.text)
+        setloading(false);
+    }
     const SelectedTemplate: TEMPLATE | undefined = Template?.find((item) => item.slug == props.params['template-slug'])
     return (
         <div className='p-5'>
             <Link href={'/dashboard'} > <Button><ArrowLeft />Back</Button></Link>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-10 p-5'>
                 {/* from section  */}
-                <FormSection SelectedTemplate={SelectedTemplate} userFormInput={(v: any) => generateAiContent(v)} />
+                <FormSection SelectedTemplate={SelectedTemplate} userFormInput={(v: any) => generateAiContent(v)} loading={loading} />
 
                 {/* outputSection  */}
                 <div className='col-span-2'>
-                    <OutputSection />
+                    <OutputSection aiOutput={aiOutput} />
                 </div>
             </div>
         </div>
--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,11 +1,19 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Button } from '@/components/ui/button';
 import { Copy } from 'lucide-react';
 
-const OutputSection = () => {
+interface Props {
+  aiOutput: string;
+}
+
+const OutputSection = ({ aiOutput }: Props) => {
   const editorRef: any = useRef();
+  useEffect(() => {
+    const editorInstance = editorRef.current.getInstance();
+    editorInstance.setMarkdown(aiOutput);
+  }, [aiOutput])
   return (
     <div className='bg-white shadow-lg border'>
       <div className='flex justify-between items-center p-5'>
