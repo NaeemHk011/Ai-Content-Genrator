6f6a27ece3e129a34dcc8e5f8ccb76d6f4b3bb67
NaeemHk011 <itxkhaskheli@gmail.com>
2024-09-07 02:03:46 +0500

    sign in div updated

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return <SignIn />
+    return <div className='flex items-center justify-center '> <SignIn /> </div>
 
 
 }
